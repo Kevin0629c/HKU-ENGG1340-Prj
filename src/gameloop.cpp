fae2855150f56cdd5445f5eec771a23fc111e34c
Kevin <158831372+Kevin0629Zcher@users.noreply.github.com>
2024-03-13 09:18:08 +0000

    intersection / break wall / fixed !

--- src/gameloop.cpp
+++ src/gameloop.cpp
@@ -76,41 +76,74 @@ Gameloop::Gameloop(int theGame_mode)
 Gameloop::~Gameloop()
 {
 }
-
+bool Gameloop::intersection(int pos_x, int pos_y, int **themap, bool the_first_move){
+    int num = 0;
+    if (themap[pos_y+1][pos_x] == 1 || themap[pos_y+1][pos_x] == 8)
+        num++;
+    if (themap[pos_y-1][pos_x] == 1 || themap[pos_y-1][pos_x] == 8)
+        num++;
+    if (themap[pos_y][pos_x+1] == 1 || themap[pos_y][pos_x+1] == 8)
+        num++;
+    if (themap[pos_y][pos_x-1] == 1 || themap[pos_y][pos_x-1] == 8)
+        num++;
+    
+    if (num >= 3 && the_first_move == false) 
+        return 0;
+    return 1;
+}
 int Gameloop::run()
 {
     int winCols = getWinCols();
     int winRows = getWinRows();
     int position[2] = {1, 1}; //row / col
     char input;
+    bool the_first_move;
     // cout << "winCols: " << winCols << endl;
     // cout << "winRows: " << winRows << endl;
     clearScreen();
     Maze2D maze(winCols / 2 - 2, (winRows % 2 == 0 ? winRows + 1 : winRows ) - 2, 0);
     maze.printMap();
     maze.map2D;
+    bool is_writing = false;
+    Timer timer(&is_writing, 0, 0);
     while (true)
     {
         char input = getch();
-        if ( maze.map2D[position[0] + lookup[input][1]][position[1] + lookup[input][0]] != 0 ){
-            printAt( 2*position[1], position[0], maze.glyphs[8]); 
-            maze.editMap(position[0], position[1], 8);
-            position[0] = position[0] + lookup[input][1];
-            position[1] = position[1] + lookup[input][0];
-            if (maze.map2D[position[0]][position[1]] == 2){
-                cout << "you win the game" << endl;
+        the_first_move = true;
+        while (intersection(position[1], position[0], maze.map2D, the_first_move)) {
+            the_first_move = false;
+            if ( maze.map2D[position[0] + lookup[input][1]][position[1] + lookup[input][0]] != 0 ){
+                printAt( 2*position[1], position[0], maze.glyphs[8]); // change the moved space 
+                maze.editMap(position[0], position[1], 8); // edit the map
+                position[0] = position[0] + lookup[input][1]; // move the player
+                position[1] = position[1] + lookup[input][0]; // move the player
+                if (maze.map2D[position[0]][position[1]] == 2){
+                    cout << "you win the game" << endl;
+                    return 0;
+                }
+                if (maze.map2D[position[0]][position[1]] == 3){
+                    printAt( 2*position[1], position[0], maze.glyphs[9]); // change the player location
+                    maze.editMap(position[0], position[1], 9);
+                    char input = getch();
+                    for (int i = 1; i < 7; i++) {
+                        if ( maze.map2D[position[0] + i*lookup[input][1]][position[1] + i*lookup[input][0]] != 2 && maze.map2D[position[0] + i*lookup[input][1]][position[1] + i*lookup[input][0]] != 8 ){
+                            if (position[1] + i*lookup[input][0] != 0 && position[0] + i*lookup[input][1] != 0 && position[1] + i*lookup[input][0] != maze.result_width -1 && position[0] + i*lookup[input][1] != maze.result_height -1 ) {
+                                printAt( 2*(position[1] + i*lookup[input][0]), position[0] + i*lookup[input][1], maze.glyphs[1]); // change the moved space 
+                                maze.editMap(position[0] + i*lookup[input][1], position[1] + i*lookup[input][0], 1); // edit the map
+                                this_thread::sleep_for(chrono::milliseconds(50));
+                            }
+                        }
+                    }
+                    break;
+                }
+                printAt( 2*position[1], position[0], maze.glyphs[9]); // change the player location
+                maze.editMap(position[0], position[1], 9);
+            }
+            else 
                 break;
-            }
-            if (maze.map2D[position[0]][position[1]] == 3){
-                
-            }
-            printAt( 2*position[1], position[0], maze.glyphs[9]);
-            maze.editMap(position[0], position[1], 9);
         }
-        
     }
-    bool is_writing = false;
-    Timer timer(&is_writing, 0, 0);
+    
     timer.start();
     this_thread::sleep_for(chrono::seconds(1));
     timer.pause();
