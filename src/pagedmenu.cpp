7b417ed4ddfcefb2c347d33d01bf66567dd99539
Jax <enhancedjax@gmail.com>
2024-04-09 11:10:19 +0800

    Update pagedmenu.cpp

--- src/pagedmenu.cpp
+++ src/pagedmenu.cpp
@@ -32,7 +32,7 @@ int PagedMenu::displayMenu()
     string quit = R"(QUIT)";                                                            // the pattern of quit
 
     frame(winCols,winRows);                                                             // the UI frame
-    printAt(1,(winRows/2)-5,R"(         
+    printAt(0,(winRows/2)-7,R"(x
 x        _    __  __    _     ________ _   _  ____    
 x       / \  |  \/  |  / \   |__  /_ _| \ | |/ ___|   
 x      / _ \ | |\/| | / _ \    / / | ||  \| | |  _    
@@ -42,11 +42,11 @@ x    | __ )|  _ \| ____|  / \  | |/ / _ \| | | |_   _|
 x    |  _ \| |_) |  _|   / _ \ | ' / | | | | | | | |  
 x    | |_) |  _ <| |___ / ___ \| . \ |_| | |_| | | |  
 x    |____/|_| \_\_____/_/   \_\_|\_\___/ \___/  |_|  
-x                                                           )");                        // the title showed in UI
+x)");                        // the title showed in UI
 
-    botton(winCols-29,(winRows/2)-5,COLOR_GREEN, play);                                // botton is a function in helers.cpp which helps 
+    botton(65,(winRows/2)-3,COLOR_GREEN, play);                                // botton is a function in helers.cpp which helps 
                                                                                        // building a thin frame around the words
-    botton(winCols-29,(winRows/2)+2,COLOR_DEFAULT,quit);                               // botton(x_coor of the word, y_coor of the word, colour , word to be showed)
+    botton(65,(winRows/2)+0,COLOR_DEFAULT,quit);                               // botton(x_coor of the word, y_coor of the word, colour , word to be showed)
 
     
 
@@ -56,16 +56,16 @@ x                                                           )");                
     while (userinput != 'D'&& userinput != 'd')                                         // D is the botton for confriming the choice
     {
 
-        botton(winCols-29,(winRows/2)-5,COLOR_DEFAULT, play);                           // changes colour when playing moving their choices
-        botton(winCols-29,(winRows/2)+2,COLOR_DEFAULT,quit);
+        botton(65,(winRows/2)-3,COLOR_DEFAULT, play);                           // changes colour when playing moving their choices
+        botton(65,(winRows/2)+0,COLOR_DEFAULT,quit);
         if (userinput == 'W' || userinput == 'w')                                       // the play botton is on the top so "W" is used to move to the top 
         {
-            botton(winCols-29,(winRows/2)-5,COLOR_GREEN, play);                         // the botton of "PLAY" changes to green when player moves the choice to that
+            botton(65,(winRows/2)-3,COLOR_GREEN, play);                         // the botton of "PLAY" changes to green when player moves the choice to that
             response = 0;                                                               // response = 0 = play
         }
         else if (userinput == 'S' || userinput == 's')                                  // the quit botton is at the bottom so "S" is used 
         {
-            botton(winCols-29,(winRows/2)+2,COLOR_RED,quit);
+            botton(65,(winRows/2)+0,COLOR_RED,quit);
             response = -1;                                                              // response = -1 = quit
         }
         userinput = getch();                                                            // getting the next userinput
